d36cee090449b1e8275f22d01390a2a326101abc
chrisn21 <chrisnguyen24@hotmail.com>
2015-01-16 18:23:05 -0800

    Initial Base Commit

---
+++ CentralizedSites/Content/js/app.js
@@ -0,0 +1,31 @@
+﻿(function() {
+    'use strict';
+
+    var app = angular.module('dezLoungeApp', []);
+
+    app.controller('LoginController', ['$scope', '$http', function($scope, $http) {
+
+        $scope.returnCredentials = function () {
+
+            $http.post('DezApi/verify/', { username: $scope.user, password: $scope.password, email: $scope.email }).
+                  success(function (data, status, headers, config) {
+                      if (data === true)
+                      {
+                          $scope.user = '';
+                          $scope.password = '';
+                          $scope.email = '';
+
+                          alert("Account was successfully verified!");
+                      }
+
+                      else {
+                          alert("Check input credentials and try again!");
+                      }
+                  }).
+                  error(function (data, status, headers, config) {
+                      alert("Unable to verify account...");
+                  });
+            }
+        }]
+    );
+})()
---
+++ CentralizedSites/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security;
+using System.Web.Http;
+
+namespace CentralizedSites.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public class UserProfile
+        {
+            public string Username;
+            public string Password;
+            public string Email;
+        }
+
+        readonly UserProfile[] _validUsers = new UserProfile[] 
+        { 
+            new UserProfile { Username = "MasterCrookz", Password = "pokemon", Email = "Kevin@Groceries.com" }, 
+            new UserProfile { Username = "ChrisWin", Password = "DezLounge", Email = "DezChris@Groceries.com" }, 
+            new UserProfile { Username = "SangOw", Password = "DezCrow", Email = "DezSang@Groceries.com" } 
+        };
+
+        [Route("DezApi/verify/")]
+        [HttpPost]
+        public bool VerifyAccount(UserProfile profile)
+        {
+            if (string.IsNullOrEmpty(profile.Username) || string.IsNullOrEmpty(profile.Password) || string.IsNullOrEmpty(profile.Email))
+                return false;
+
+            var verifyConnection = new SqlConnection("Data Source=Chris-PC;Initial Catalog=master;User ID=dezadmin;Password=123sqldez321");
+
+            var query = "SELECT * from AccountProfiles where Username = '" + profile.Username + "' AND Password = '" + profile.Password + "' AND Email = '" + profile.Email + "'";
+            var verify = new SqlCommand(query, verifyConnection);
+            verifyConnection.Open();
+
+            using (var results = verify.ExecuteReader())
+            {
+                return results.HasRows;
+            }
+
+            //return _validUsers.Any(vp => profile.Username.Equals(vp.Username) && profile.Password.Equals(vp.Password) && profile.Email.Equals(vp.Email));
+        }
+    }
+}
